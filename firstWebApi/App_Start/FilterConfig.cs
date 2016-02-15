1dff5b650383ff64f33d34ef78be4d69467d25bf
xiaotank <329873423@qq.com>
2016-02-15 09:43:59 +0800

    1111

---
+++ firstWebApi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace firstWebApi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
