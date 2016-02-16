3e6c21f0ae5d54db2365de0ccef406b5a6c260dc
xiaotank <329873423@qq.com>
2016-02-16 20:58:32 +0800

    tty

--- firstWebApi/Controllers/ValuesController.cs
+++ firstWebApi/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace firstWebApi.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/valuesztztztztguoguoguoguo
+        // GET api/valuesguoguoguoguo
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
